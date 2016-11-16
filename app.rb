8f105811fba478e619db32c1e2955d523236ea6f
Alexa <alexa.anderson@outlook.com>
2016-11-16 16:32:04 -0600

    Work on pages

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 require 'sinatra'
 require 'sinatra/reloader'
-require 'figaro'
 require 'csv'
 require 'thin'
 
@@ -10,16 +9,8 @@ require_relative 'lib/dice_jobs_page_parser'
 require_relative 'lib/job_writer'
 require_relative 'lib/dice_scraper_controller'
 
-# Figaro.application = Figaro::Application.new(
-#   environment: 'development',
-#   path: File.expand_path('../config/application.yml', __FILE__)
-# )
-# Figaro.load
-#
-# key = ENV['dice']
 
 get '/' do
-  # search
   jobs = CSV.read("test.csv")
   erb :index, locals: { jobs: jobs }
 end
@@ -30,7 +21,5 @@ post '/' do
 
   jobs = DiceScraperController.new.search(keyword, location)
   JobWriter.new.save_results('test.csv', jobs)
-  # invokes scraper
-    # writes csv
   redirect '/'
 end
--- lib/job_writer.rb
+++ lib/job_writer.rb
@@ -4,11 +4,9 @@ class JobWriter
 
   def save_results(filepath, results)
 
-    headers = !File.exist?(filepath)
-
     CSV.open(filepath, 'w') do |csv|
       # each one of these comes out in its own row.
-      csv << ["Job Title", "Company", "Link", "Location", "Posting Date", "Company ID", "Job ID"] if headers
+      csv << ["Job Title", "Company", "Link", "Location", "Posting Date", "Company ID", "Job ID"]
 
       results.each do |job|
         csv << job.to_a
