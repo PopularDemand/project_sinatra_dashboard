79fea8de1670fef1f27b8a2094e3f2b0fd3b9c53
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-11-16 16:06:40 -0700

    Some changes

--- lib/company_profiler.rb
+++ lib/company_profiler.rb
@@ -1,1 +1,29 @@
-<a href='https://www.glassdoor.com/index.htm'>powered by <img src='https://www.glassdoor.com/static/img/api/glassdoor_logo_80.png' title='Job Search' /></a>
+require 'httparty'
+require 'pp'
+
+ID = ENV["GLASSDOOR_ID"]
+KEY = ENV["GLASSDOOR_KEY"]
+IP = ENV["MY_IP"]
+USER_AGENT = "Windows%20Chrome"
+
+def build_url(terms)
+  base_url = "http://api.glassdoor.com/api/api.htm?"
+  parameters = "t.p=#{ID}&t.k=#{KEY}&userip=#{IP}&useragent=#{USER_AGENT}&format=json&v=1&action=employers"
+  query = "&q=#{terms}"
+  url = "#{base_url}#{parameters}#{query}"
+end
+
+url = build_url("Kane Partners")
+
+p url
+
+response = HTTParty.get(url)
+
+pp response
+
+
+response["response"]["employers"]["exactMatch"]
+
+response["response"]["employers"]["cultureAndValuesRating"]
+
+response["response"]["employers"]["featuredReview"]
